83f6960a49711a0ea4876f6a3ecf05153155e78f
Nickel Yadav <nickelyadav57@gmail.com>
2024-04-24 14:50:35 +0530

    :sparkles: feat: Adds routes for all endpoints

--- nodejs-api-design/src/server.ts
+++ nodejs-api-design/src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import router from "./router";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.status = 200;
-  res.json({ message: "Hell" });
+  res.json({ message: "Hello" });
 });
+
+app.use("/api", router);
 
 export default app;
