2742c513cd39deb1f6db82697d28cd03c53e6787
Nickel Yadav <nickelyadav57@gmail.com>
2024-04-25 15:37:03 +0530

    :passport_control: feat: Improvesauth to check for JWT authenticity

--- nodejs-api-design/src/modules/auth.ts
+++ nodejs-api-design/src/modules/auth.ts
@@ -8,11 +8,29 @@ export const createJWT = (user) => {
   return token;
 };
 
-export const protect = (req, res) => {
+export const protect = (req, res, next) => {
   const bearer = req.headers.authorization;
+
   if (!bearer) {
     res.status(401);
     res.json({ message: "not authorized" });
+    return;
+  }
+  const [, token] = bearer.split(" ");
+
+  if (!token) {
+    res.status(401);
+    res.json({ message: "Not valid token" });
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401);
+    res.json({ message: "Not valid token" });
     return;
   }
 };
